3a0b60f5377e1e714ec685547d0d72b7577530ad
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-05-17 11:52:21 -0700

    homepage and sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import Layout from './pages/Layout';
-import Home from './pages/Home';
+
 
 const App = () => {
   return (
-    <Layout>
-      <Home />
-    </Layout>
+    <>
+    <Layout />
+    </>
+   
   );
 };
 
