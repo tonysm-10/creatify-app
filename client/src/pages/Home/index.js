34c63a1e8b4f3156f4f29d3146d0133f2c704fc6
tonysm-10 <ayalaarellanoanthony@gmail.com>
2023-05-17 18:30:52 -0700

    activated link color when on page

--- client/src/pages/Home/index.js
+++ client/src/pages/Home/index.js
@@ -13,7 +13,7 @@ return(
         <p className='info'>Welcome to our customizable e-commerce platform! Create and manage your own tailored e-commerce website. Sign up, choose a theme, and customize it with your brand colors and logo. Easily add products with images, descriptions, prices, and organize them into categories for seamless navigation.</p>
         <h3>Let's get Started</h3>
         <p>The first step to get started is adding your products and creating an enticing display on your e-commerce website.</p>
-        <button className="btn" style={{ cursor: 'pointer' }} type="submit" onClick={handleButtonClick}>Get Started</button>
+        <button className="btn1" style={{ cursor: 'pointer' }} type="submit" onClick={handleButtonClick}>Get Started</button>
     </div>
 )
 }
